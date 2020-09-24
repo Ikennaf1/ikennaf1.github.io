668b75454d4181c94a3e23d6536aef4eaf34866c
Ike Felix <55805592+Ikennaf1@users.noreply.github.com>
2020-09-24 09:44:10 +0100

    Update light-box.js

--- scripts/light-box.js
+++ scripts/light-box.js
@@ -3,6 +3,10 @@ var content_1 = '\
 <div id="portfolio"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg></div>\
 <div id="name_text"><h1 class="name-text"><a href="">Ike Felix</a></h1></div>\
 <h3 class="text-title">September 2020</h3>\
+    <p class="p-highlight">Blinds Beta now available in <a href="https://wordpress.org/" target="_blank">WordPress.org plugin repository</a></p>\
+    <p class="p-text">Current version at this time: Beta version 0.0.5. <br>Blinds is a simple WordPress plugin that gives WordPress users the option to activate Night / Dark mode on\
+    WordPress blogs or websites.</p>\
+    <p class="p-text"><a href="https://wordpress.org/plugins/blinds" target="_blank">Get the plugin from WordPress</a><br>\
     <p class="p-highlight">Launched Blinds Beta</p>\
     <p class="p-text">A simple WordPress plugin that gives WordPress users the option to activate Night / Dark mode on\
     WordPress blogs or websites.</p>\
